4ca60b67907085a267e03367401f009587489852
root <root@ihongwindows.cafe24.com>
2022-12-11 14:42:08 +0900

    newFlask commit 2022.12.11 15:00

---
+++ runProject.sh
@@ -0,0 +1,5 @@
+export FLASK_APP=pybo
+export FLASK_ENV=production
+export FLASK_DEBUG=true
+export APP_CONFIG_FILE=/root/uttec/linuxFlask/config/production.py 
+. ./myproject/bin/activate
---
+++ runTest.sh
@@ -0,0 +1,9 @@
+. runProject.sh
+
+gunicorn  --bind unix:/tmp/myproject.sock 'pybo:create_app()'
+
+sudo systemctl restart nginx
+
+
+
+
